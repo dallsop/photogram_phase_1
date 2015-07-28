b1991e7faec98b40313380af9432b689b46ae312
dallsop <david.allsop@chicagobooth.edu>
2015-07-27 19:21:26 -0500

    HW5 photogram

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,24 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    photo_index = params['id']
+    @photo_source = Photo.find(photo_index).source
+    @photo_caption = Photo.find(photo_index).caption
+    render('show')
+  end
+
+  def destroy
+    photo_index = params['id']
+    i = Photo.find(photo_index)
+    i.destroy
+    @list_of_photos = Photo.all
+    render('index')
+  end
+
+  def new_form
+    render('new_form.html.erb')
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+  get("/photos/new",       { :controller => "photos", :action => "new_form" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
 end
